4d92603a2ac635f2a689daea3cfaa18915bb95c0
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-10-08 23:39:26 +0530

    working of routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <main>
+        <Routes>
+          <Route path="/movie/:id" element={<h1>Movie Information</h1>} />
+          <Route path="/actors/:id" element={<h1>Actors</h1>} />
+          <Route path="/" element={<h1>Movies</h1>} />
+          <Route path="/movies/:id" element={<h1>Profile</h1>} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
